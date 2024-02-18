40d7b3550f987e875efa7dacc2aae3ff22d08a22
Steph Crown <emmanuelstephen024@gmail.com>
2024-02-18 15:53:35 +0100

    Add individual housing details

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -118,6 +118,7 @@ export class HousingService {
   }
 
   getHousingLocationById(id: number): Housinglocation | undefined {
+    console.log({ numeric: id });
     return this.housingLocationList.find(
       (housingLocation) => housingLocation.id === id
     );
